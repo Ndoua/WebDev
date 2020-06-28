69e738f18c2a488d5454965ef6df6fc1e87de1fc
ndoua <serge.ndoua@gmail.com>
2020-06-28 15:23:19 +0200

    settings devise errors message with toastr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     flash.each_with_object([]) do |(type, message), flash_messages|
       type = 'success' if type == 'notice'
       type = 'error' if type == 'alert'
-      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true, closeDuration: 100 })</script>"
+      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
       flash_messages << text.html_safe if message
     end.join("\n").html_safe
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: {minimum: 50}
+    # validates :name, presence: true, length: { minimum: 50 }
 end
